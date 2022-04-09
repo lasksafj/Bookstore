7c6071adce1dff3b54e376b4a771dfd95fbd31ca
khoa208 <khoadodev@gmail.com>
2022-04-09 13:53:48 -0700

    asfasf

--- Khoa/AddNewAuthorPopUp.py
+++ Khoa/AddNewAuthorPopUp.py
@@ -10,6 +10,7 @@ def delete():
    yearOfDeath.delete(0, 'end')
    description.delete('1.0', 'end')
 
+# Create Tk instance
 addAuthorForm = Tk()
 addAuthorForm.resizable(0, 0)  # Lock screen
 addAuthorForm.title("Add New Author")
@@ -31,7 +32,7 @@ descriptionLabel = Label(addAuthorForm, text="Description").grid(row=6,column=0,
 description = Text(addAuthorForm, width=30, height=6)
 
 # Create buttons
-cancelButton = Button(addAuthorForm, text="Cancel")
+cancelButton = Button(addAuthorForm, text="Cancel", command=addAuthorForm.destroy)
 cancelButton.grid(row=12,column=0,columnspan=2,padx=10,pady=5,ipadx=10,sticky="W")
 clearButton = Button(addAuthorForm, text="Clear", command=delete)
 clearButton.grid(row=12,column=1,columnspan=2,pady=5,ipadx=10,sticky="W")
@@ -45,5 +46,14 @@ firstName.grid(row=3,column=1)
 yearOfBirth.grid(row=4,column=1)
 yearOfDeath.grid(row=5,column=1)
 description.grid(row=6,column=1)
+
+# Go to next entry box if you press "Enter"
+def go_to_next_entry(event, entry_list, this_index):
+    next_index = (this_index + 1) % len(entry_list)
+    entry_list[next_index].focus_set()
+
+entries = [child for child in addAuthorForm.winfo_children() if isinstance(child, Entry)]
+for idx, entry in enumerate(entries):
+    entry.bind('<Return>', lambda e, idx=idx: go_to_next_entry(e, entries, idx))
 
 addAuthorForm.mainloop()
