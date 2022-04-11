32e06197901bb4de80560f21962c795ba3884543
Quoc1509 <trunghuynh159@gmail.com>
2022-04-11 14:43:06 -0700

    quoc

--- index.py
+++ index.py
@@ -6,11 +6,16 @@ window.title("Bookstore")
 window.geometry("600x700")
 window.iconphoto(True, PhotoImage(file="logo.png"))
 
+
+
 image1 = ImageTk.PhotoImage(Image.open('background.png'))
 back_ground = Label(window, image=image1)
 back_ground.place(x=0, y=0)
 
-label = Label(window, text="WELLCOME TO BOOKSTORE APP", fg="red")
+frame = LabelFrame(window, text="Bookstore", padx=5, pady=5)
+frame.pack(padx=10, pady=10)
+
+label = Label(frame, text="WELLCOME TO BOOKSTORE APP", fg="red")
 label.config(font=("Helvatical bold", 20))
 label.pack(pady=20)
 
