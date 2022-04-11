d719db82e67fa146a2c3f2d386ad2be7e268d099
nnguyen1206 <70247531+nnguyen1206@users.noreply.github.com>
2022-04-11 14:40:28 -0700

    nhat

--- dbsql.py
+++ dbsql.py
@@ -15,8 +15,6 @@ def create_connection(db_file):
 
     return con
 
-con = create_connection("example.db")
-
 def close_connection(con):
     """ close the connection to the database
     """
@@ -37,13 +35,6 @@ def insert_db(con, table_name:str, value:list) -> None:
         con.commit()
     except Exception as e:
         print(str(e))
-
-insert = ['1156lord',
-        'Lord of the rings','J. R. R. Tolkien',
-        'nxb',2000,2,
-        100,300,'new','available',
-        'fiction']
-insert_db(con, "books", insert)
 
 def search_db(con, table_name:str, search:dict) -> list:
     """ search data in the database
@@ -67,12 +58,6 @@ def search_db(con, table_name:str, search:dict) -> list:
         print(str(e))
     return result
 
-search = {
-    'title': 'Harry Potter',
-    'author': 'JK Rowling',
-}
-print(search_db(con, 'books', search))
-
 def update_db(con, table_name:str, id:str, update:dict) -> None:
     """ update data in the database
     :con: connection
@@ -95,13 +80,6 @@ def update_db(con, table_name:str, id:str, update:dict) -> None:
     except Exception as e:
         print(str(e))
 
-# update = {
-#     "year": 2010,
-#     "price": 200
-# }
-#
-# update_db(con, "books", "1234harr", update)
-
 def delete_db(con, table_name:str, id_list:list) -> None:
     """ delete data in the database
     :con: connection
@@ -117,6 +95,30 @@ def delete_db(con, table_name:str, id_list:list) -> None:
     except Exception as e:
         print(str(e))
 
+# --------------------------------------------------
+con = create_connection("bookstore.db")
+
+# insert = ['1111harr',
+#         'Harry Potter','JK Rowling',
+#         'nxb',2000,2,
+#         100,300,'new','available',
+#         'fiction', '1-1-2000', 'n/a', 'n/a']
+# insert_db(con, "books", insert)
+#
+# search = {
+#     'cost': 100
+# }
+# print(search_db(con, 'books', search))
+#
+
+# update = {
+#     "cost": 150,
+#     'retail': 500
+# }
+#
+# update_db(con, "books", "1111harr", update)
+#
+#
 id_list = ["1156lord", "1334lord"]
 delete_db(con, "books", id_list)
 
--- example.py
+++ example.py
@@ -62,48 +62,102 @@
 #     app.render()
 #     app.mainloop()
 
-import tkinter as tk
-from PIL import Image, ImageTk
+# import tkinter as tk
+# from PIL import Image, ImageTk
+#
+# class View(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.title("Calculator")
+#         self.state("zoomed")
+#         self.render()
+#
+#     def onSumBtn(self):
+#         a = int(self.entry1.get())
+#         b = int(self.entry2.get())
+#         c = a + b
+#         self.label3.config(text=c)
+#
+#     def render(self):
+#         for i in range(4):
+#             self.columnconfigure(i, weight=1)
+#         for i in range(3):
+#             self.rowconfigure(i, weight=1)
+#
+#         label1 = tk.Label(self, text="number 1", fg="red")
+#         label1.grid(row=0, column=1)
+#         label2 = tk.Label(self, text="number 2", fg="red")
+#         label2.grid(row=1, column=1)
+#
+#         self.entry1 = tk.Entry(self)
+#         self.entry1.grid(row=0, column=2)
+#         self.entry2 = tk.Entry(self)
+#         self.entry2.grid(row=1, column=2)
+#
+#         button1 = tk.Button(self, text="click me", command=self.onSumBtn)
+#         button1.grid(row=2, column=1)
+#
+#         self.label3 = tk.Label(self, text='')
+#         self.label3.grid(row=2, column=2)
+#
+#         self.image1 = ImageTk.PhotoImage(Image.open('logo.png'))
+#         tk.Label(self, image=self.image1).grid(row=0, column=0)
+#
+#
+# if __name__ == "__main__":
+#     app = View()
+#     app.mainloop()
 
-class View(tk.Tk):
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Calculator")
-        self.state("zoomed")
-        self.render()
 
-    def onSumBtn(self):
-        a = int(self.entry1.get())
-        b = int(self.entry2.get())
-        c = a + b
-        self.label3.config(text=c)
+        self.title('Treeview demo')
+        self.geometry('620x200')
 
-    def render(self):
-        for i in range(4):
-            self.columnconfigure(i, weight=1)
-        for i in range(3):
-            self.rowconfigure(i, weight=1)
+        self.tree = self.create_tree_widget()
 
-        label1 = tk.Label(self, text="number 1", fg="red")
-        label1.grid(row=0, column=1)
-        label2 = tk.Label(self, text="number 2", fg="red")
-        label2.grid(row=1, column=1)
+    def create_tree_widget(self):
+        columns = ('first_name', 'last_name', 'email')
+        tree = ttk.Treeview(self, columns=columns, show='headings')
 
-        self.entry1 = tk.Entry(self)
-        self.entry1.grid(row=0, column=2)
-        self.entry2 = tk.Entry(self)
-        self.entry2.grid(row=1, column=2)
+        # define headings
+        tree.heading('first_name', text='First Name')
+        tree.heading('last_name', text='Last Name')
+        tree.heading('email', text='Email')
 
-        button1 = tk.Button(self, text="click me", command=self.onSumBtn)
-        button1.grid(row=2, column=1)
+        tree.bind('<<TreeviewSelect>>', self.item_selected)
+        tree.grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.label3 = tk.Label(self, text='')
-        self.label3.grid(row=2, column=2)
+        # add a scrollbar
+        scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=tree.yview)
+        tree.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky='ns')
 
-        self.image1 = ImageTk.PhotoImage(Image.open('logo.png'))
-        tk.Label(self, image=self.image1).grid(row=0, column=0)
+        # generate sample data
+        contacts = []
+        for n in range(1, 100):
+            contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+        # add data to the treeview
+        for contact in contacts:
+            tree.insert('', tk.END, values=contact)
+
+        return tree
+
+    def item_selected(self, event):
+        for selected_item in self.tree.selection():
+            item = self.tree.item(selected_item)
+            record = item['values']
+            # show a message
+            showinfo(title='Information', message=','.join(record))
 
 
-if __name__ == "__main__":
-    app = View()
+if __name__ == '__main__':
+    app = App()
     app.mainloop()
